108f0a757753afb75e7f9e33978ffdf92076084a
Muhammad Farooq <m.farooq.h7@gmail.com>
2020-09-19 10:39:03 +0500

    Updates

---
+++ precache-manifest.d5d70fcec5be3025fd322b4ef529f5ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb19189458145e9539a6446d785cbc4",
+    "url": "/expense-tracker-by-farooq/index.html"
+  },
+  {
+    "revision": "27ed2750882c9cd37bae",
+    "url": "/expense-tracker-by-farooq/static/css/main.558648cf.chunk.css"
+  },
+  {
+    "revision": "b5eaae746c9f47562a40",
+    "url": "/expense-tracker-by-farooq/static/js/2.eceafb4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/expense-tracker-by-farooq/static/js/2.eceafb4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ed2750882c9cd37bae",
+    "url": "/expense-tracker-by-farooq/static/js/main.f4804f5f.chunk.js"
+  },
+  {
+    "revision": "3cefdc1946dbca0b6f9a",
+    "url": "/expense-tracker-by-farooq/static/js/runtime-main.9d42a3ce.js"
+  }
+]);
